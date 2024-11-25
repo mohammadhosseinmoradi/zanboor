29329d5db4c9c52c47f87e89e3725736e4b59134
mohammadhosseinmoradi <mhmdev64@gmail.com>
2024-11-26 00:05:03 +0330

    chore: disallow some page in robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,9 +1,11 @@
 import { MetadataRoute } from "next";
+import { routes } from "@/lib/constants/routes";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
+      disallow: [routes.profile, routes.messages, routes.favorites, routes.search],
     },
   };
 }
