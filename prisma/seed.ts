45657d3702425ffb7572f7e0bb16eac359caadb4
mohammadhosseinmoradi <mhmdev64@gmail.com>
2024-12-11 17:54:14 +0330

    Add database seed script and Prisma schema updates

---
+++ prisma/seed.ts
@@ -0,0 +1,71 @@
+import {
+  PrismaClient,
+  MaritalStatus,
+  Gender,
+  SkinColor,
+  EducationLevel,
+  EmploymentStatus,
+} from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.upsert({
+    where: {
+      phone: "+989922819367",
+    },
+    update: {
+      phone: "+989922819367",
+    },
+    create: {
+      phone: "+989922819367",
+      profile: {
+        create: {
+          personal: {
+            create: {
+              firstName: "محمد حسین",
+              lastName: "مرادی",
+              displayName: "محمد",
+              image: "",
+              gender: Gender.Male,
+              birthdate: new Date(),
+              maritalStatus: MaritalStatus.Single,
+            },
+          },
+          physicalAttributes: {
+            create: {
+              height: 167,
+              skinColor: SkinColor.Wheatish,
+              weight: 54,
+            },
+          },
+          educationAndCareer: {
+            create: {
+              educationLevel: EducationLevel.Diploma,
+              employmentStatus: EmploymentStatus.Employed,
+              jobTitle: "توسعه دهنده وب",
+              fieldOfStudy: "کامپیوتر",
+            },
+          },
+        },
+      },
+    },
+    include: {
+      profile: {
+        include: {
+          personal: true,
+        },
+      },
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (err) => {
+    console.error(err);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
